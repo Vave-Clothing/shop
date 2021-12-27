5dae2e653b3e61aa9157b790cab8347a48b6c532
DerLev <levin.schroeren@gmail.com>
2021-12-28 00:15:39 +0100

    storing stripe sessions in DB for later access

--- pages/api/checkout_sessions/stripe.ts
+++ pages/api/checkout_sessions/stripe.ts
@@ -2,6 +2,8 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import Joi from 'joi'
 import validate from '@/lib/middlewares/validation'
 import Stripe from 'stripe'
+import Order from '@/schemas/Order'
+import dbConnect from '@/lib/dbConnect'
 
 const schema = Joi.object({
   cart: Joi.array().items(Joi.object({
@@ -17,17 +19,25 @@ const stripe = new Stripe(process.env.STRIPE_SK!, {
 
 export default validate({ body: schema }, async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
+    await dbConnect()
+
     try {
-      const cartItems = req.body.cart.map((i:any) => {
-        return { price: i.id, quantity: i.quantity }
-      })
+      const cartItems = await Promise.all(req.body.cart.map(async (i:any) => {
+        const resPrice = await stripe.prices.retrieve(i.id)
+        const resAmount = resPrice.unit_amount
+        return { price: i.id, quantity: i.quantity, amount: resAmount! / 100 }
+      }))
 
       const shippingRateId: string = req.body.shipping
+
+      const stripeItems = cartItems.map((i:any) => {
+        return { price: i.price, quantity: i.quantity }
+      })
 
       const params: Stripe.Checkout.SessionCreateParams = {
         mode: 'payment',
         payment_method_types: [ 'card', 'sepa_debit', 'giropay', 'sofort', 'klarna' ],
-        line_items: cartItems,
+        line_items: stripeItems,
         success_url: `${req.headers.origin}/success?pid={CHECKOUT_SESSION_ID}&platform=stripe`,
         cancel_url: req.body.cancelUrl ? req.headers.origin + req.body.cancelUrl : `${req.headers.origin}/checkout/payment`,
         shipping_address_collection: {
@@ -38,6 +48,40 @@ export default validate({ body: schema }, async (req: NextApiRequest, res: NextA
         submit_type: 'pay',
       }
       const checkoutSession: Stripe.Checkout.Session = await stripe.checkout.sessions.create(params)
+
+      const dbItems = cartItems.map((i:any) => {
+        return { id: i.price, quantity: i.quantity, price: i.amount }
+      })
+
+      const shippingRateAmount = await (await stripe.shippingRates.retrieve(shippingRateId)).fixed_amount?.amount || 0
+
+      const order = new Order({
+        platform: 'stripe',
+        pid: checkoutSession.id,
+        purchased_items: dbItems,
+        total_price: checkoutSession.amount_total! / 100,
+        email: 'john.doe@example.com',
+        shipping_address: {
+          name: 'John Doe',
+          line1: 'Platz der Republik 1',
+          zip: 11011,
+          city: 'Berlin',
+          country: 'DE',
+        },
+        shipping_rate: {
+          id: shippingRateId,
+          price: shippingRateAmount / 100
+        },
+        status: 'pending',
+        stripePI: checkoutSession.payment_intent
+      })
+
+      try {
+        await order.save()
+      } catch(err) {
+        console.log(err)
+        return res.status(500).send({ code: 500, message: 'Internal Server Error' })
+      }
 
       res.status(200).json(checkoutSession)
     } catch (err: any) {
