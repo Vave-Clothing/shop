dc1e3263e2dec9f69412fcc9dd14d21fa18a8eae
DerLev <33765680+DerLev@users.noreply.github.com>
2021-12-27 20:52:10 +0100

    beginning to work on webhooks (#4)

--- types/env.d.ts
+++ types/env.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     NEXT_PUBLIC_STRIPE_PK: string
     STRIPE_SK: string
+    STRIPE_WS: string
     NEXT_PUBLIC_PAYPAL_CID: string
     PAYPAL_CS: string
     MONGODB_URI: string
